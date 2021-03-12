e0dee3c2fefb8dae36915e483c37a00bdb02bb02
Wadzinski, Galen L <gwadzinski@aiconline.com>
2021-03-12 11:59:31 -0600

    Revert "Added MD5 to long"
    This reverts commit ffe25bd98dc4d9b79bd8257dc3c76f00463566dc.

---
+++ CloneDtoTests/TestHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Serialization;
+using System.IO;
+
+namespace CloneDtoTests
+{
+    public class TestHelper
+    {
+
+        public static string SerializeObject<T>(T toSerialize)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(toSerialize.GetType());
+            StringWriter textWriter = new StringWriter();
+            xmlSerializer.Serialize(textWriter, toSerialize);
+            return textWriter.ToString();
+        }
+
+    }
+}
